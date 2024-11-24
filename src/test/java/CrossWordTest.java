effabeed082e2adfe69da2416e148fdd117bc3e8
RUPAMDEVELOPER1 <99700899+RUPAMDEVELOPER1@users.noreply.github.com>
2024-11-24 10:53:02 +0530

    dhange

--- src/test/java/CrossWordTest.java
+++ src/test/java/CrossWordTest.java
@@ -56,54 +56,53 @@ public class CrossWordTest {
         List<Integer> discounted_price=new ArrayList<>();
         List<Integer> non_discounted_price=new ArrayList<>();
         List<String> book_names=new ArrayList<>();
-        for(int i=1;i<=products.size();i++)
-        {
-            String template = "(//div[@class='wizzy-product-item-price '])[{i}]";
-            String val=String.valueOf(i);
-            String message = template.replace("{i}",val);
-            //System.out.println(message);
-            List<WebElement> is_discount = driver.findElements(By.xpath(message+"//span"));
-            String book="(//p[@class='product-item-title'])[{i}]";
-            String book1=String.valueOf(i);
-            String book_name = template.replace("{i}",book1);
-
-            if(is_discount.size()>0)
-            {
-                String value=is_discount.get(0).getText();
-                value.trim();
-                StringBuilder sb = new StringBuilder(value);
-                // Remove "world" by index
-                sb.delete(0,1);
-                int price=Integer.parseInt(String.valueOf(sb));
-                discounted_price.add(price);
-                //System.out.println(price);
-                //discounted price book names
-                String books_name_xpath = "(//p[@class='product-item-title'])[{i}]";
-                String book_xpath = template.replace("{i}",String.valueOf(i));
-                String books_name=driver.findElement(By.xpath(book_xpath)).getText().trim();
-                book_names.add(books_name);
-                System.out.println(books_name);
-            }
-            else
-            {
-                //String s=driver.findElement(By.xpath(message)).getText().trim();
-                //StringBuilder sb = new StringBuilder(s);
-                // Remove "world" by index
-                //sb.delete(0,1);
-                //int price=Integer.parseInt(String.valueOf(sb));
-                //non_discounted_price.add(price);
-                //System.out.println(price);
-
-            }
-
-        }
-        List<Integer> copied_list=new ArrayList<>(non_discounted_price);
-        Collections.sort(copied_list);
-        System.out.println("Original List: " + non_discounted_price);
-        System.out.println("Copied and Sorted List: " + copied_list);
-        Assert.assertEquals(non_discounted_price,copied_list,"The non discounted price are not in sorted order");
-        List<String> copied_booklist=new ArrayList<>(book_names);
-        System.out.println("Discounted book names: " + copied_booklist);
+//        for(int i=1;i<=products.size();i++)
+//        {
+//            String template = "(//div[@class='wizzy-product-item-price '])[{i}]";
+//            String val=String.valueOf(i);
+//            String message = template.replace("{i}",val);
+//            //System.out.println(message);
+//            List<WebElement> is_discount = driver.findElements(By.xpath(message+"//span"));
+//            String book="(//p[@class='product-item-title'])[{i}]";
+//            String book1=String.valueOf(i);
+//            String book_name = template.replace("{i}",book1);
+//
+//            if(is_discount.size()>0)
+//            {
+//                String value=is_discount.get(0).getText();
+//                value.trim();
+//                StringBuilder sb = new StringBuilder(value);
+//                // Remove "world" by index
+//                sb.delete(0,1);
+//                int price=Integer.parseInt(String.valueOf(sb));
+//                discounted_price.add(price);
+//                //System.out.println(price);
+//                //discounted price book names
+//                String books_name_xpath = "(//p[@class='product-item-title'])[{i}]";
+//                String book_xpath = template.replace("{i}",String.valueOf(i));
+//                String books_name=driver.findElement(By.xpath(book_xpath)).getText().trim();
+//                book_names.add(books_name);
+//                System.out.println(books_name);
+//            }
+//            else
+//            {
+//                String s=driver.findElement(By.xpath(message)).getText().trim();
+//                StringBuilder sb = new StringBuilder(s);
+//                sb.delete(0,1);
+//                int price=Integer.parseInt(String.valueOf(sb));
+//                non_discounted_price.add(price);
+//                System.out.println(price);
+//
+//            }
+//
+//        }
+//        List<Integer> copied_list=new ArrayList<>(non_discounted_price);
+//        Collections.sort(copied_list);
+//        System.out.println("Original List: " + non_discounted_price);
+//        System.out.println("Copied and Sorted List: " + copied_list);
+//        Assert.assertEquals(non_discounted_price,copied_list,"The non discounted price are not in sorted order");
+//        List<String> copied_booklist=new ArrayList<>(book_names);
+//        System.out.println("Discounted book names: " + copied_booklist);
 
     }
     @Test
@@ -120,7 +119,7 @@ public class CrossWordTest {
     {
         driver.quit();
         System.out.println("my name is rupam");
-        
+
     }
 
 }
